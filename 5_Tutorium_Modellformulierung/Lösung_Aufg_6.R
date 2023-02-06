207d1d977bd87bb10639740c6bc751d87ac2460b
Belinda Fleischmann <fl.belinda@googlemail.com>
2023-02-06 11:08:21 +0100

    Update

--- 5_Tutorium_Modellformulierung/Lösung_Aufg_6.R
+++
@@ -1,5 +0,0 @@
-"Dieses Skript simuliert die Unverzerrtheit des Varianzparameterschätzers im ALM Szenario
-von  unabhängigen und identisch normalverteilten Zufallsvariablen.
-
-Lösung zu Aufg. 6 der Selbstkontrollfragen für 
-6_Modellschätzung (ALM SoSe 2022)"
